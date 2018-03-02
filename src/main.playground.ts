fc7917f30963e465164d353c70b9299db1092b50
landough <orlando_wilson@msn.com>
2018-03-02 11:00:32 -0500

    angular playground configured

---
+++ src/main.playground.ts
@@ -0,0 +1,4 @@
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { initializePlayground, PlaygroundModule } from 'angular-playground';
+initializePlayground('app-root');
+platformBrowserDynamic().bootstrapModule(PlaygroundModule);
